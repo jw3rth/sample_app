12ed6f15a127ec0921bf55625cf9393772883352
Jason <Jason@ProgressiveComputingLLC.com>
2014-09-26 15:54:25 -0700

    Added layout links with routes Also ammended helper function

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root :to => "pages#home"
 
+  match '/contact', :to => 'pages#contact', :via =>[:get]
+  match '/about', :to  => 'pages#about' , :via =>[:get]
+  match '/help' , :to => 'pages#help' , :via =>[:get]
+
+
+  #get 'pages/home'
   get 'pages/contact'
-
   get 'pages/about'
+  get 'pages/help'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
